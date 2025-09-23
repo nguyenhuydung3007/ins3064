fabf672aa5a0dd9d5bf12c3b7a9b488866fdcea9
nguyenhuydung3007 <nguyenhuydung3072005@gmail.com>
2025-09-23 15:34:32 +0700

    Create index_3.php

---
+++ index_3.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <h1>This is my first PHP file</h1>
+    <table border = "1">
+        <?php
+            for($i = 0; $i < 5; $i++) {
+                echo "
+                <tr>
+                    <td>$i<td>
+                <tr>
+                ";
+            }
+        ?>
+    </table>
+</body>
+</html>
