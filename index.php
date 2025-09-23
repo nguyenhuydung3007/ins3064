89f48b14d5c0833774d045c354fe774c09e46e98
nguyenhuydung3007 <nguyenhuydung3072005@gmail.com>
2025-09-23 14:32:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,19 @@
 </head>
 <body>
     <?php 
-    $name = "Mr. A";
-    $age = 20;
-    $courses = array("Java", "C", "PHP");
-    echo "Name: " . $name . ", age: ".$age. "<br/>3rd course is:" . $courses[2];
+    // $name = "Mr. A";
+    // $age = 20;
+    // $courses = array("Java", "C", "PHP");
+    // echo "Name: " . $name . ", age: ".$age. "<br/>3rd course is:" . $courses[2];
+        $x = 10;
+        $y = 11;
+        echo "x: " . $x . "</br>";
+        echo "x: " . $y . "</br>";
+        echo "x/y: " .($x/$y) . "</br>";
+        echo "x%y: " .($x%$y) . "</br>";
+        echo "x++: " .($x++) . "</br>";
+        echo "++y: " .(++$y) . "</br>";
     ?>
+
 </body>
 </html>
