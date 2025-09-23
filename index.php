18ab8c5c106a36700c8a1c649c50995a058bc3b1
nguyenhuydung3007 <nguyenhuydung3072005@gmail.com>
2025-09-23 13:42:12 +0700

    Update index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>MY file PHP</title>
+</head>
+<body>
+    <?php 
+    $name = "Mr. A";
+    $age = 20;
+    $courses = array("Java", "C", "PHP");
+    echo "Name: " . $name . ", age: ".$age. "<br/>3rd course is:" . $courses[2];
+    ?>
+</body>
+</html>
